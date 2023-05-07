d45aa433c0d1a88cb76aa2b32774b005595c35ea
Jess <jess.m.jerez@gmail.com>
2023-05-07 16:59:50 -0600

    add seed file

--- index.js
+++ index.js
@@ -16,44 +16,46 @@ function start() {
         type: "list",
         message: "What would you like to do?",
         choices: [
-            "View all departments", 
-            "View all roles", 
-            "View all employees", 
-            "Add a department", 
-            "Add a role", 
-            "Add an employee", 
-            "Update employee role", 
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update employee role",
             "Exit"
         ],
-    }).then(function (answer) {
-        switch (answer.action) {
-            case "View all departments":
-                viewDepartments();
-                break
-            case "View all roles":
-                viewRoles();
-                break
-            case "View all employees":
-                viewEmployees();
-                break 
-            case "Add a department":
-                addDepartment();
-                    break
-            case "Add a role":
-                addRole();
-                    break
-             case "Add an employee":
-                addEmployee();
-                    break      
-            case "Update employee role":
-                updateEmployeeRole();
-                break
-            case "Exit":
-                connection.end();
-                    break
-    
-        }
     })
-}
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View all departments":
+                    viewDepartments();
+                    break
+                case "View all roles":
+                    viewRoles();
+                    break
+                case "View all employees":
+                    viewEmployees();
+                    break
+                case "Add a department":
+                    addDepartment();
+                    break
+                case "Add a role":
+                    addRole();
+                    break
+                case "Add an employee":
+                    addEmployee();
+                    break
+                case "Update employee role":
+                    updateEmployeeRole();
+                    break
+                case "Exit":
+                    connection.end();
+                    break;
+                default:
+                    break;
+            }
+        });
+};
 
-start()
+start();
---
+++ seeds.sql
@@ -0,0 +1,11 @@
+USE employees_db;
+
+INSERT INTO department (name) VALUES
+("Sales"), 
+("Human Resources");
+
+INSERT INTO role (title, salary, department_id) VALUES
+("Web Developer", 90000, 1)
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+("Antonia", "Solar", 1, )
